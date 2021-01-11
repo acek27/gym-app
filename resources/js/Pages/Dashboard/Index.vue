853d1fe9cbff5057840f53adc50827d5ef071351
Razak Syaiful Rochman <razak.syaiful.r@gmail.com>
2021-01-11 13:28:40 +0700

    handleLogout

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -1,11 +1,21 @@
 <template>
-    <div>
-        <p>Welcome, {{fname}} {{lname}}</p>
-    </div>
+    <layout>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <p>Welcome, {{ fname }} {{ lname }}</p>
+                </div>
+            </div>
+        </div>
+    </layout>
 </template>
 
 <script>
+import Layout from "../../Shared/Layout";
+
 export default {
-    props:['fname','lname']
+    components: {Layout},
+
+    props: ['fname', 'lname']
 }
 </script>
