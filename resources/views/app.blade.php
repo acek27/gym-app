f5e8ba3539a9e5a2bf805359ecacb635692597d4
Razak Syaiful Rochman <razak.syaiful.r@gmail.com>
2021-03-09 11:04:27 +0700

    background & particlejs

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,8 +6,158 @@
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet"/>
     <script src="{{ mix('/js/app.js') }}" defer></script>
     @routes
+    <style>
+        canvas {
+            display: block;
+            vertical-align: bottom;
+        }
+
+        #particles-js {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            background-image: url('/images/bg.jpg');
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-position: center;
+            background-size: cover;
+        }
+
+    </style>
 </head>
 <body>
+<div id="particles-js"></div>
 @inertia
 </body>
+<script src="{{asset('js/particle.js')}}"></script>
+<script>
+    particlesJS("particles-js", {
+        "particles": {
+            "number": {
+                "value": 80,
+                "density": {
+                    "enable": true,
+                    "value_area": 700
+                }
+            },
+
+
+            "color": {
+                "value": "#ffffff"
+            },
+
+            "shape": {
+                "type": "circle",
+                "stroke": {
+                    "width": 0,
+                    "color": "#000000"
+                },
+
+                "polygon": {
+                    "nb_sides": 5
+                }
+            },
+
+
+            "opacity": {
+                "value": 0.5,
+                "random": false,
+                "anim": {
+                    "enable": false,
+                    "speed": 0.1,
+                    "opacity_min": 0.1,
+                    "sync": false
+                }
+            },
+
+
+            "size": {
+                "value": 3,
+                "random": true,
+                "anim": {
+                    "enable": false,
+                    "speed": 10,
+                    "size_min": 0.1,
+                    "sync": false
+                }
+            },
+
+
+            "line_linked": {
+                "enable": true,
+                "distance": 150,
+                "color": "#ffffff",
+                "opacity": 0.4,
+                "width": 1
+            },
+
+            "move": {
+                "enable": true,
+                "speed": 2,
+                "direction": "none",
+                "random": false,
+                "straight": false,
+                "out_mode": "out",
+                "bounce": false,
+                "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                }
+            }
+        },
+
+
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "grab"
+                },
+
+                "onclick": {
+                    "enable": true,
+                    "mode": "push"
+                },
+
+                "resize": true
+            },
+
+            "modes": {
+                "grab": {
+                    "distance": 140,
+                    "line_linked": {
+                        "opacity": 1
+                    }
+                },
+
+
+                "bubble": {
+                    "distance": 400,
+                    "size": 40,
+                    "duration": 2,
+                    "opacity": 8,
+                    "speed": 3
+                },
+
+                "repulse": {
+                    "distance": 200,
+                    "duration": 0.4
+                },
+
+                "push": {
+                    "particles_nb": 4
+                },
+
+                "remove": {
+                    "particles_nb": 2
+                }
+            }
+        },
+
+
+        "retina_detect": true
+    });
+</script>
 </html>
